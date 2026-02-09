1a0b9cb8f65e7b9d26617ca994593db21bc98163
DESKTOP-9TEKMTN\kosta <h551654@naver.com>
2026-02-09 16:04:36 +0900

    시나리오 3번 - GoodsService

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -4,4 +4,9 @@ public class GoodsService {
 	public void insert() {
 		System.out.println("insert call .....");
 	}
+	
+	public void select() {
+		System.out.println("select 추가..");
+		System.out.println("시나리오3번 테스트");
+	}
 }
