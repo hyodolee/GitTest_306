1f31f7b5588f9b4b468f4fd52004f6b58c8f8734
DESKTOP-9TEKMTN\kosta <h551654@naver.com>
2026-02-09 17:08:31 +0900

    충돌해결

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -3,7 +3,8 @@ package kosta.controller;
 public class GoodsController {
   int i = 10;
   public void test(){
-    System.out.println("원격에서 test 추가함 - 시나리오 5번 테스트");
+
+	  System.out.println("합친결과");
   }
   public void selectAll(){
   }
