1cce022c478e4fa880695b62fbe0d14d9306de03
DESKTOP-9TEKMTN\kosta <h551654@naver.com>
2026-02-09 16:41:25 +0900

    로컬에서 메인수정함

--- src/kosta/view/MainView.java
+++ src/kosta/view/MainView.java
@@ -5,6 +5,8 @@ public class MainView {
 	public static void main(String[] args) {
 		System.out.println("test...........");
 		System.out.println("수정....");
+		
+		System.out.println("메인에 수정함.");
 	}
 
 }
